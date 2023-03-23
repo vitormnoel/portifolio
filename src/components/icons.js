93dc1f9aebbf2b6ae3ed7ffc961e00e75d920ee2
Vitor Manoel <65787376+vitormnoel@users.noreply.github.com>
2023-03-23 16:43:38 -0700

    Update icons.js

--- src/components/icons.js
+++ src/components/icons.js
@@ -5,7 +5,7 @@ function Icons() {
     <ul className="py-4 flex flex-row sm:flex-col sm:gap-5 gap-10 justify-center">
       <ListIcon
         icon="resume"
-        href="https://drive.google.com/file/d/1-xa0EdFRJnIBhaGe6CTDiRaZe3a8Ew8J/view?usp=sharing"
+        href="https://drive.google.com/file/d/19S5XdDtsk98SnZqkpSvf9luLGdwGcfPf/view?usp=sharing"
       />
       <ListIcon
         icon="linkedin"
